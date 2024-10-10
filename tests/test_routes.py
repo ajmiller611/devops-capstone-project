3e169c0846fbb6c963885c0635e212dd29c0641f
ajmiller611 <ajmiller611@live.com>
2024-10-10 12:32:30 -0400

    added update account functionality

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -134,9 +134,40 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
 
-    def test_account_not_found(self):
+    def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         response = self.client.get(
+            f"{BASE_URL}/0", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        response = self.client.get(
+            f"{BASE_URL}/0", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_account(self):
+        """It should Update an exisiting Account"""
+        test_account = AccountFactory()
+        response = self.client.post(
+            BASE_URL,
+            json=test_account.serialize(),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        account = response.get_json()
+        account["name"] = "John Smith"
+        response = self.client.put(f"{BASE_URL}/{account['id']}", json=account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "John Smith")
+
+    def test_update_account_not_found(self):
+        """It should not Update an Account that is not found"""
+        response = self.client.put(
             f"{BASE_URL}/0", content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
